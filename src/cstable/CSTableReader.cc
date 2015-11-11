674630edc692a11c4f7bed1df56eedc8bbc2c19e
Paul Asmuth <paul@paulasmuth.com>
2015-11-11 17:06:14 +0100

    CSTableReader::openFile stub

---
+++ src/cstable/CSTableReader.cc
@@ -0,0 +1,22 @@
+/**
+ * This file is part of the "libcstable" project
+ *   Copyright (c) 2015 Paul Asmuth, FnordCorp B.V.
+ *
+ * libcstable is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v3.0. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#include <cstable/CSTableReader.h>
+#include <cstable/v1/CSTableReader.h>
+
+namespace stx {
+namespace cstable {
+
+RefPtr<CSTableReader> CSTableReader::openFile(const String& filename) {
+  return new v1::CSTableReader(filename);
+}
+
+} // namespace cstable
+} // namespace stx
+
